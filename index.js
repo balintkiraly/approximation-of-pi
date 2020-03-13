c2568d347c8b77a74c38459ca52cf3809d79fcd2
kiralybalint <kiraly.balint@simonyi.bme.hu>
2020-03-14 00:25:46 +0100

    Create a circle

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function setup() {}
+const canvasWidth = 640;
+const canvasHeight = canvasWidth;
 
-function draw() {
-  ellipse(50, 50, 80, 80);
-}
+setup = () => {
+  createCanvas(canvasWidth, canvasHeight);
+};
+
+draw = () => {
+  const cirecleSize = min(canvasWidth, canvasHeight) - 50;
+  fill(255, 200, 200);
+  stroke(127, 0, 0);
+  ellipse(canvasWidth / 2, canvasHeight / 2, cirecleSize, cirecleSize);
+};
