2a9a483979ba98df567b6f603312c188e1a61dbf
kiralybalint <kiraly.balint@simonyi.bme.hu>
2020-03-14 01:06:32 +0100

    Calculate pi with buil-in random number generator

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const canvasWidth = 640;
 const canvasHeight = canvasWidth;
+const cirecleRadius = Math.floor(Math.min(canvasWidth, canvasHeight) / 2);
+const origoX = Math.floor(canvasWidth / 2);
+const origoY = Math.floor(canvasHeight / 2);
 
 setup = () => {
   createCanvas(canvasWidth, canvasHeight);
 };
 
 draw = () => {
-  const cirecleSize = min(canvasWidth, canvasHeight) - 50;
-  fill(255, 200, 200);
-  stroke(127, 0, 0);
-  ellipse(canvasWidth / 2, canvasHeight / 2, cirecleSize, cirecleSize);
+  createPoints();
+};
+
+let inside = 0;
+let outside = 0;
+
+createPoints = () => {
+  for (let i = 0; i < 1000; i++) {
+    const x = Math.random() * canvasWidth;
+    const y = Math.random() * canvasHeight;
+
+    const d = Math.sqrt(Math.pow(x - origoX, 2) + Math.pow(y - origoY, 2));
+
+    if (cirecleRadius > d) {
+      inside++;
+      stroke(0, 255, 0);
+    } else {
+      outside++;
+      stroke(255, 200, 200);
+    }
+    strokeWeight(2);
+    point(x, y, 10);
+  }
+  console.log("value of PI: ", 4 * (inside / (outside + inside)));
 };
